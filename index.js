00bbb5f42c165bad17d0699e21ebaf0b47d5ec04
Tanya22bose <tanyabose56@gmail.com>
2025-06-05 08:17:13 +0530

    add: weather app UI | integration with weather api | assets

---
+++ index.js
@@ -0,0 +1,43 @@
+async function fetchData(target) {
+  try {
+    const url = `http://api.weatherapi.com/v1/current.json?key=a1a84770072844b88ea142523253005&q=${target}&aqi=no`;
+    const response = await fetch(url); //fetch returns a promise
+    const {
+      current: {
+        temp_c,
+        condition: { text, icon },
+      },
+      location: { localtime, country },
+    } = await response.json();
+
+    //get dom nodes
+    const temp = document.querySelector(".temp");
+    const location = document.querySelector(".location");
+    const timeDate = document.querySelector(".time-date");
+    const weatherIcon = document.querySelector(".condition-img");
+    const conditionTag = document.querySelector(".condition");
+    const event = new Date(localtime);
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
+    //update the dom nodes
+    temp.innerHTML = temp_c;
+    location.innerHTML = country;
+    timeDate.innerHTML = event.toLocaleDateString(undefined, options);
+    weatherIcon.src = icon;
+    conditionTag.innerHTML = text;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const target = document.querySelector(".search-input");
+  fetchData(target.value);
+});
